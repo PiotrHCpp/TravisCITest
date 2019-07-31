15dc0870fcac7116fba2ab233962fb787124f708
Piotr Hoffner <piotr.hoffner.wroc@gmail.com>
2019-07-31 20:50:48 +0200

    changed filename, updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TravisCITest
+
+## Build status: [![Build Status](https://travis-ci.com/PiotrHCpp/TravisCITest.svg?branch=master)](https://travis-ci.com/PiotrHCpp/TravisCITest)
--- palindrome.cpp
+++ palindrome.cpp
@@ -17,13 +17,12 @@ bool isPalindrome(const std::string_view &input)
 int main()
 {
     std::cout << "Palindrome function - stl style iterators version.\n";
-    std::cout << "Enter string for palindrome check: ";
-    std::string string1;
-    std::cin >> string1;
+    std::string string1 = "kajak";
+    std::cout << "string1 = " << "\"" << string1 << "\"\n";
     if(isPalindrome(string1))
-        std::cout << "\"" << string1 << "\" " << "is a palindrome!\n";
+        std::cout << "\"" << string1 << "\" is a palindrome!\n"; 
     else
-        std::cout << "\"" << string1 << "\" " << "is NOT a palindrome!\n";
+        std::cout << "\"" << string1 << "\" is NOT a palindrome!\n"; 
     return 0;
 }
 
