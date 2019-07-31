ae26963a918732a50e8e0c02c538eff35d41090c
Piotr Hoffner <piotr.hoffner.wroc@gmail.com>
2019-07-31 12:28:21 +0200

    added working check palindrome function

---
+++ palindrome_stl.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+bool isPalindrome(const std::string_view &input)
+{
+    auto ritr = input.rbegin();
+    for(auto itr = input.begin(); itr != input.end(); ++itr)
+    {
+        if(*itr != *ritr)
+            return false;
+        ++ritr;
+    }
+    return true;
+}
+
+int main()
+{
+    std::cout << "Enter string for palindrome check: ";
+    std::string string1;
+    std::cin >> string1;
+    if(isPalindrome(string1))
+        std::cout << "\"" << string1 << "\" " << "is a palindrome!\n";
+    else
+        std::cout << "\"" << string1 << "\" " << "is NOT a palindrome!\n";
+    return 0;
+}
+
+
+
